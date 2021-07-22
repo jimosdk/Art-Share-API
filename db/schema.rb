68aa6dde5fff179d645d1a7604434a8e2c1b5c09
jimosk <jimosdk@gmail.com>
2021-07-22 17:51:18 +0300

    Added column favorite to artwork_shares table for determining which of the artworks that were shared with a specific user are his favorites

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_22_142110) do
+ActiveRecord::Schema.define(version: 2021_07_22_143125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_07_22_142110) do
   create_table "artwork_shares", force: :cascade do |t|
     t.integer "viewer_id", null: false
     t.integer "artwork_id", null: false
+    t.boolean "favorite", default: false, null: false
     t.index ["artwork_id", "viewer_id"], name: "index_artwork_shares_on_artwork_id_and_viewer_id", unique: true
     t.index ["viewer_id"], name: "index_artwork_shares_on_viewer_id"
   end
