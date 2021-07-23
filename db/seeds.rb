1e57dfc21c8f5ff8a9285b95a7c54872d12bd62d
jimosk <jimosdk@gmail.com>
2021-07-23 16:41:57 +0300

    Added example seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,16 +11,39 @@ ActiveRecord::Base.transaction do
     Artwork.destroy_all
     ArtworkShare.destroy_all
 
+
+    #pablo
     u1  = User.create!(username:"Pablo")
-    u2 = User.create!(username:"Dean")
-    u3 = User.create!(username:"Sam")
 
     a1 = Artwork.create!(artist_id:u1.id,image_url:"pablo image url 1",title:"Pablo's art 1")
     a2 = Artwork.create!(artist_id:u1.id,image_url:"pablo image url 2",title:"Pablo's art 2")
+        
+    #dean
+    u2 = User.create!(username:"Dean")
+
+    #sam
+    u3 = User.create!(username:"Sam")
+
     a3 = Artwork.create!(artist_id:u3.id,image_url:"sam image url 1",title:"Sam's art 1")
 
-    ArtworkShare.create!(viewer_id: u2.id,artwork_id: a1.id)
-    ArtworkShare.create!(viewer_id: u3.id,artwork_id: a1.id)
-    ArtworkShare.create!(viewer_id: u2.id,artwork_id: a2.id)
-    ArtworkShare.create!(viewer_id: u2.id,artwork_id: a3.id)
+    #nami
+    u4 = User.create!(username:"Nami")
+
+    a4 = Artwork.create!(artist_id:u4.id,image_url:"nami image url 1",title:"Nami's art 1")
+    a5 = Artwork.create!(artist_id:u4.id,image_url:"nami image url 2",title:"Nami's art 2")
+    a6 = Artwork.create!(artist_id:u4.id,image_url:"nami image url 3",title:"Nami's art 3")
+
+
+    #shared with dean
+    ArtworkShare.create!(viewer_id: u2.id,artwork_id: a1.id) #pablo 1
+    ArtworkShare.create!(viewer_id: u2.id,artwork_id: a2.id) #pablo 2
+    ArtworkShare.create!(viewer_id: u2.id,artwork_id: a3.id) #sam 1
+
+    #shared with sam
+    ArtworkShare.create!(viewer_id: u3.id,artwork_id: a1.id) #pablo1
+
+    #shared with nami
+    ArtworkShare.create!(viewer_id: u4.id,artwork_id: a2.id) #pablo2
+    ArtworkShare.create!(viewer_id: u4.id,artwork_id: a3.id) #sam 1
+    
 end
